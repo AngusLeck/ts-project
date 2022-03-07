101331ac526dc1f6af6597ce7a0cc8a929ccef82
Angus Leck <aleck42@gmail.com>
2022-03-07 12:20:19 +1100

    feat: add greeting script

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 1. install dependencies `yarn`
 2. run tests `yarn jest`
+3. run greeting script `yarn do greetings`
--- src/HelloWorld/HelloWorld.ts
+++ src/HelloWorld/HelloWorld.ts
@@ -1,7 +1,7 @@
 export class HelloWorld {
-  private greeting = "Hello World";
+  private greeting;
 
-  constructor(greetingMessage: string) {
+  constructor(greetingMessage: string = "Hello World") {
     this.greeting = greetingMessage;
   }
 
